1d8a1b5adfe9d070a4dfc2065fb5630d96e38274
Jimi Carlo Wills <jimi@cpan.org>
2013-08-26 10:30:47 +0000

    Import of JIMI/Statistics-TheilSen-0.03 from CPAN.
    gitpan-cpan-distribution: Statistics-TheilSen
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JIMI/Statistics-TheilSen-0.03.tar.gz
    gitpan-cpan-author:       JIMI
    gitpan-cpan-maturity:     released

--- lib/Statistics/TheilSen.pm
+++ lib/Statistics/TheilSen.pm
@@ -17,11 +17,11 @@ Statistics::TheilSen - Perl implementation of Theil Sen Estimator
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
